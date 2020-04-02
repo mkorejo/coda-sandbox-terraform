c5ac36fe2896960244138c2c5b5c5151c070b620
Murad Korejo <muradkorejo@Murads-2nd-MacBook-Pro.local>
2020-04-02 16:58:33 -0500

    add eks tag to vpc artifacts

--- workspaces/muradkorejo/main.tf
+++ workspaces/muradkorejo/main.tf
@@ -5,7 +5,12 @@ module "sandbox_vpc" {
 
   name = local.prefix
   cidr = "10.0.0.0/16"
-  tags = local.tags
+  tags = merge(
+    local.tags,
+    {
+      "kubernetes.io/cluster/muradkorejo-eks" = "shared"
+    }
+  )
 
   azs             = ["us-east-1c", "us-east-1d", "us-east-1f"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
