b9dc108c1f32c0e1a2264bfc4258eb031fc78e57
Murad Korejo <muradkorejo@Murads-2nd-MacBook-Pro.local>
2020-04-02 01:12:20 -0500

    drop us-east-1e

--- workspaces/muradkorejo/main.tf
+++ workspaces/muradkorejo/main.tf
@@ -6,7 +6,7 @@ module "sandbox_vpc" {
   name = local.prefix
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1d", "us-east-1e", "us-east-1f"]
+  azs             = ["us-east-1c", "us-east-1d", "us-east-1f"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
