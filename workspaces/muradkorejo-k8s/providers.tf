1e6babb32c06da21777104c751da4395c4412fd6
Murad Korejo <muradkorejo@Murads-2nd-MacBook-Pro.local>
2020-04-02 20:29:30 -0500

    fixes

--- workspaces/muradkorejo-k8s/providers.tf
+++ workspaces/muradkorejo-k8s/providers.tf
@@ -1,14 +1,20 @@
 data "aws_eks_cluster" "sandbox_eks" {
-  name = var.cluster_name
+  name = var.cluster_name != "" ? var.cluster_name : join("-", [local.prefix, "eks"])
 }
 
 data "aws_eks_cluster_auth" "sandbox_eks" {
-  name = var.cluster_name
+  name = var.cluster_name != "" ? var.cluster_name : join("-", [local.prefix, "eks"])
+}
+
+provider "aws" {
+  version = "2.56.0"
+
+  region = "us-east-1"
 }
 
 provider "kubernetes" {
-  host                   = "${data.aws_eks_cluster.sandbox_eks.endpoint}"
-  cluster_ca_certificate = "${base64decode(data.aws_eks_cluster.sandbox_eks.certificate_authority.0.data)}"
-  token                  = "${data.aws_eks_cluster_auth.sandbox_eks.token}"
+  host                   = data.aws_eks_cluster.sandbox_eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.sandbox_eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.sandbox_eks.token
   load_config_file       = false
 }
