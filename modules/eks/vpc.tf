4a974fcf24f0cf25afae00789329a1ab21be6241
Murad Korejo <mkorejo@gmail.com>
2020-04-04 22:46:48 -0500

    add name tag to remote access sg

--- modules/eks/vpc.tf
+++ modules/eks/vpc.tf
@@ -1,7 +1,9 @@
 resource "aws_security_group" "eks_cluster_node_group_remote_access_sg" {
   name        = join("-", [aws_eks_cluster.eks_cluster.name, var.node_group_name, "remote-access"])
   description = "SSH access for EKS worker nodes"
-  tags        = var.tags
+  tags        = merge(var.tags, {
+    "Name" = join("-", [aws_eks_cluster.eks_cluster.name, var.node_group_name, "remote-access"])
+  })
   vpc_id      = var.vpc_id
 
   ingress {
