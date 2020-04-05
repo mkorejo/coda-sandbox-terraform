d3b72e14eaf6dc4686ee4dbacee9cf28b14d7704
Murad Korejo <mkorejo@gmail.com>
2020-04-04 20:22:24 -0500

    and crds helm release

--- workspaces/muradkorejo-k8s/main.tf
+++ workspaces/muradkorejo-k8s/main.tf
@@ -4,6 +4,13 @@ resource "kubernetes_namespace" "argocd" {
   }
 }
 
+resource "helm_release" "crds" {
+  name = "crds"
+  repository = "http://mkorejo.github.io/helm_repo"
+  chart = "crds"
+  namespace = "kube-system"
+}
+
 resource "helm_release" "argocd" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
