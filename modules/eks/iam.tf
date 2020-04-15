e8f403779f411fa11a84eb8626c81ad3da7dd9d4
Murad Korejo <mkorejo@gmail.com>
2020-04-15 11:15:01 -0500

    add service account iam to eks module

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -56,3 +56,40 @@ resource "aws_iam_role_policy_attachment" "eks_worker_role-AmazonEC2ContainerReg
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_worker_role.name
 }
+
+# Service Account IAM
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = []
+  url             = "${aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer}"
+}
+
+data "aws_iam_policy_document" "service_account_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = ["${aws_iam_openid_connect_provider.eks_oidc.arn}"]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_external_dns_role" {
+  name = join("-", [var.prefix, "eks-external-dns-role"])
+  tags = var.tags
+
+  assume_role_policy = "${data.aws_iam_policy_document.service_account_assume_role_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_external_dns_role-external-dns-route53-policy" {
+  policy_arn = "arn:aws:iam::458527324684:policy/external-dns-route53-policy"
+  role       = aws_iam_role.eks_external_dns_role.name
+}
--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,6 +2,10 @@ output "eks_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
+output "eks_external_dns_role_arn" {
+  value = aws_iam_role.eks_external_dns_role.arn
+}
+
 output "eks_kubeconfig_ca_data" {
   value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
 }
