f6dfa0698984c16b73db79d76c9cf3c890804e89
Murad Korejo <mkorejo@gmail.com>
2020-04-24 20:35:03 -0500

    adjust iam_pod config

--- modules/eks/iam_pod.tf
+++ modules/eks/iam_pod.tf
@@ -1,17 +1,19 @@
 # Setup IAM Roles for Kubernetes Service Accounts
 # https://www.terraform.io/docs/providers/aws/r/eks_cluster.html#enabling-iam-roles-for-service-accounts
 # https://docs.aws.amazon.com/eks/latest/userguide/iam-roles-for-service-accounts-technical-overview.html
-data "aws_iam_policy_document" "service_account_assume_role_policy" {
+data "aws_iam_policy_document" "external_dns_assume_role_policy" {
   statement {
     actions    = ["sts:AssumeRole"]
     effect     = "Allow"
 
     principals {
-      identifiers = ["arn:aws:iam::458527324684:role/muradkorejo-eks-external-dns"]
+      identifiers = [join("", ["arn:aws:iam:::role/", var.prefix, "eks-external-dns"])]
       type        = "AWS"
     }
   }
+}
 
+data "aws_iam_policy_document" "service_account_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
@@ -41,20 +43,42 @@ resource "aws_iam_openid_connect_provider" "eks_oidc" {
 }
 
 # cert-manager and ExternalDNS
+# https://cert-manager.io/docs/configuration/acme/dns01/route53/
+# https://github.com/kubernetes-sigs/external-dns/blob/master/docs/tutorials/aws.md
 resource "aws_iam_role" "eks_external_dns_role" {
   name = join("-", [var.prefix, "eks-external-dns"])
   tags = var.tags
 
-  assume_role_policy = data.aws_iam_policy_document.service_account_assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.external_dns_assume_role_policy.json
 }
 
-# ***** This policy attachment is dependent on an existing IAM policy in the account. *****
-resource "aws_iam_role_policy_attachment" "eks_external_dns_role-external-dns-route53-policy" {
-  policy_arn = "arn:aws:iam::458527324684:policy/external-dns-route53-policy"
-  role       = aws_iam_role.eks_external_dns_role.name
+resource "aws_iam_role_policy" "external_dns" {
+  name   = join("-", [var.prefix, "eks-external-dns"])
+  role   = aws_iam_role.eks_external_dns_role.name
+  policy = jsonencode({
+    Statement = [
+      {
+        Action   = ["route53:GetChange"],
+        Effect   = "Allow",
+        Resource = "arn:aws:route53:::change/*"
+      },
+      {
+        Action   = ["route53:ChangeResourceRecordSets"]
+        Effect   = "Allow",
+        Resource = "arn:aws:route53:::hostedzone/*"
+      },
+      {
+        Action   = ["route53:ListHostedZones", "route53:ListResourceRecordSets"],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+    Version = "2012-10-17"
+  })
 }
 
 # Vault
+# https://github.com/hashicorp/vault-guides/blob/master/operations/aws-kms-unseal/terraform-aws/instance-profile.tf
 resource "aws_iam_role" "eks_vault_unseal_role" {
   name = join("-", [var.prefix, "eks-vault-unseal"])
   tags = var.tags
