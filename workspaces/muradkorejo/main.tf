574e30f4110a9c55790af9dc9e0e8fdc63f1179a
Murad Korejo <mkorejo@gmail.com>
2020-10-08 15:16:36 -0500

    update vpc module version

--- workspaces/muradkorejo/main.tf
+++ workspaces/muradkorejo/main.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/2.32.0
 module "sandbox_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.32.0"
+  version = "2.57.0"
 
   name = local.prefix
   cidr = "10.0.0.0/16"
