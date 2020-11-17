c0ba1ad74a02d2612bf558e1125ad0a7deaf4cfb
Murad Korejo <mkorejo@gmail.com>
2020-11-17 17:10:34 -0600

    add minikue-k8s config

---
+++ workspaces/minikube-k8s/main.tf
@@ -0,0 +1,64 @@
+#########################
+######### Setup #########
+#########################
+
+# minikube start --addons dashboard --addons eks --addons ingress --addons ingress-dns --addons metrics-server
+
+provider "helm" {
+  kubernetes { config_context = "minikube" }
+}
+
+provider "kubernetes" { config_context = "minikube" }
+
+terraform {
+  backend "remote" {
+    organization = "muradkorejo"
+
+    workspaces {
+      name = "minikube-k8s"
+    }
+  }
+}
+
+#########################
+####### Resources #######
+#########################
+
+resource "helm_release" "crds" {
+  name       = "crds"
+  repository = "https://mkorejo.github.io/helm-charts"
+  chart      = "crds"
+  version    = "0.3.1"
+  namespace  = "kube-system"
+}
+
+resource "kubernetes_namespace" "namespaces" {
+  for_each = toset([
+    "cattle-system",
+    "cert-manager"
+  ])
+  metadata { name = each.value }
+}
+
+resource "helm_release" "cert-manager" {
+  name       = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  version    = "v0.15.0-alpha.2"
+  namespace  = "cert-manager"
+  depends_on = [ helm_release.crds, kubernetes_namespace.namespaces ]
+}
+
+resource "helm_release" "rancher" {
+  name       = "rancher"
+  repository = "https://releases.rancher.com/server-charts/stable"
+  chart      = "rancher"
+  version    = "2.5.2"
+  namespace  = "cattle-system"
+  depends_on = [ helm_release.crds, helm_release.cert-manager, kubernetes_namespace.namespaces ]
+
+  set {
+    name  = "hostname"
+    value = "rancher.minikube"
+  }
+}
