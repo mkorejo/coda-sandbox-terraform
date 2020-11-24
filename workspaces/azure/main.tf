a5b6085e53cf0c8be23372db24a4e328f9041446
Murad Korejo <mkorejo@gmail.com>
2020-11-24 10:32:31 -0600

    does not like the name

--- workspaces/azure/main.tf
+++ workspaces/azure/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_subnet" "internal" {
 #########################
 
 resource "azurerm_storage_account" "main" {
-  name                     = "default"
+  name                     = "mkorejo"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
