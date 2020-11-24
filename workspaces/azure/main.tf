22df1379fcc95fa1afec89451288a87228031ec0
Murad Korejo <mkorejo@gmail.com>
2020-11-24 10:09:38 -0600

    upgrade to Standard sku for public_ip

--- workspaces/azure/main.tf
+++ workspaces/azure/main.tf
@@ -61,6 +61,7 @@ resource "azurerm_public_ip" "lb" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_lb" "lb" {
